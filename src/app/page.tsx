ecd3ba5bed74ea0244814f743459f53770d6e365
licraw <lcrawshaw@fender.com>
2024-11-07 15:58:04 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function HomePage() {
   return (
     <div>
       <Banner
+        ctaLink="/contact"
         imagePath="https://paintpdx.com/wp-content/uploads/2018/02/iStock-592031050.jpg"
         cta="Get Estimate"
         description="Portland Painting and Restoration is one of Portland and Vancouver’s premier repair, light remodel, and finish contractors."
