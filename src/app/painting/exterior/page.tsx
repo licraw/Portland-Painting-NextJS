eb9f8250bba99381d1c9f4e19c1f05ac176fff56
licraw <lcrawshaw@fender.com>
2025-02-13 17:51:48 -0800

    update

--- src/app/painting/exterior/page.tsx
+++ src/app/painting/exterior/page.tsx
@@ -1,6 +1,8 @@
-import ServiceHero from "../../components/ServiceHero";
-import ServiceIntro from "../../components/ServiceIntro";
 import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CarouselGallery from "../../components/ImageGallery/CarouselGallery";
+import imageFiles from "@/app/gallery/exterior/galleryFiles";
 
 export const metadata: Metadata = {
   title: "Exterior Painting Services | Portland Painting & Restoration",
@@ -37,28 +39,166 @@ export const metadata: Metadata = {
 };
 
 export default function ExteriorPaintingPage() {
+  const imageSet1 = imageFiles.slice(0, 12);
+  const imageSet2 = imageFiles.slice(12, 24);
   return (
     <>
       <div>
-        {/* Hero Section */}
-        <ServiceHero
-          title="Exterior Painting Services"
-          subtitle="Enhancing curb appeal with durable, weather-resistant finishes."
-          backgroundImage="/gallery/exterior1.jpeg"
-        />
+        <div className="p-8 pl-6 lg:pl-20 lg:pr-20">
+          <div className="flex items-center space-x-3 bg-gray-100 py-2 px-4 rounded-full max-w-fit mb-4">
+            <div className="flex items-center justify-center w-8 h-6 bg-white rounded-full">
+              <Image
+                src="/gallery/leaf.svg"
+                alt="Portland Painting and Restoration Logo"
+                width={16}
+                height={16}
+              />
+            </div>
+            <p className="text-sm font-medium text-gray-600">
+              Get a free estimate today
+              <Link
+                href="/estimate"
+                className="underline hover:no-underline hover:text-gray-800 transition ml-1"
+              >
+                here!
+              </Link>
+            </p>
+          </div>
+          <h1
+            style={{
+              fontFamily: '"Helvetica Neue", Arial, sans-serif',
+              fontWeight: 500,
+              lineHeight: "1.2",
+            }}
+            className="font-sans font-medium text-4xl lg:text-7xl leading-[1.2]"
+          >
+            Exterior Residential Painting
+          </h1>
+          <p className="pb-4 pt-4">
+            Welcome to Portland Painting and Restoration, your premier
+            destination for exceptional exterior painting services in Portland,
+            Oregon. With our team of skilled professionals and a commitment to
+            delivering outstanding results, we are the go-to choice for all your
+            painting needs.
+          </p>
+          <p className="pb-4 pt-4">
+            In the beautiful Pacific Northwest, our region’s unique weather
+            patterns pose challenges for maintaining the exterior of your
+            property. The constant moisture and unpredictable climate can lead
+            to structural damage if left unchecked. That’s why it’s crucial to
+            invest in high-quality exterior painting services to protect your
+            home or business.
+          </p>
+          <p className="pb-4 pt-4">
+            At Portland Painting and Restoration, we understand the importance
+            of proper surface preparation for a long-lasting paint coating. Our
+            meticulous process starts with pressure washing, removing dirt and
+            mildew to ensure a clean canvas for the new paint. This step is
+            crucial as it allows the paint to bond effectively, resulting in a
+            durable and flawless finish. Additionally, we offer comprehensive
+            carpentry services to address any structural issues before painting,
+            ensuring that your property’s exterior is not only beautiful but
+            also sound and secure. Our expert carpenters are skilled in repairs
+            and enhancements, providing a seamless integration with our painting
+            services to give your property a complete makeover.
+          </p>
+        </div>
+      </div>
+      <CarouselGallery images={imageSet1} />
+      <div className="p-8 pl-6 lg:pl-20 lg:pr-20 light-green-bg">
+        <p className="pb-4 pt-4">
+          Our skilled painters are experts in surface preparation, meticulously
+          scraping, sanding, caulking, and priming your property. We take pride
+          in our attention to detail, knowing that thorough preparation is the
+          foundation for a beautiful and long-lasting paint job. By sealing any
+          bare wood and addressing gaps that can contribute to moisture
+          intrusion, we provide optimal protection against the elements.
+        </p>
+        <p className="pb-4 pt-4">
+          When it comes to choosing the right paint, we only work with
+          top-quality materials that can withstand the challenging climate of
+          Portland. Our extensive range of paints and coatings ensures that we
+          can meet your specific needs, whether you desire vibrant colors or a
+          more subdued finish. We tailor our services to enhance the aesthetic
+          appeal of your property while also providing durable protection
+          against the elements.
+        </p>
+        <p className="pb-12 pt-4">
+          Customer satisfaction is at the heart of our business. After the final
+          coat of paint is expertly applied, our dedicated team conducts a
+          thorough walk-through with you to ensure your complete satisfaction.
+          We address any concerns or touch-ups that may be required, ensuring
+          that the end result exceeds your expectations.
+        </p>
+      </div>
+      <CarouselGallery bgcolor="#e8f2ec" images={imageSet2} />
+      <div className="p-8 pl-6 lg:pl-20 lg:pr-20 light-green-bg pt-0">
+        <div className="pl-6 lg:pl-20 lg:pr-20 pb-8">
+          <h2 className="font-sans font-medium text-4xl lg:text-4xl leading-[1.2] pb-8 text-center">
+            Exterior Painting Services
+          </h2>
+          <div className="max-w-4xl mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <ul className="list-disc list-inside text-left space-y-2 w-full max-w-[90%] mx-auto md:mx-0 md:max-w-full">
+                <li>Minimum 25yr Warranty Paint</li>
+                <li>Pressure Washing &amp; Gutter Cleaning</li>
+                <li>Carpentry repairs</li>
+                <li>Mildew &amp; Algae Remediation</li>
+                <li>Masking &amp; Protection</li>
+                <li>Sanding (w/lead dust collection)</li>
+              </ul>
 
-        {/* Intro Section */}
-        <ServiceIntro heading="Our Exterior Painting Services">
-          <p className="mb-8">
-            Your home&apos;s exterior is its first impression, and a high-quality paint job can make it shine. Our exterior painting services are designed to protect your home from the elements while delivering a stunning visual upgrade.
-          </p>
-          <p className="mb-8">
-            We begin by preparing the surfaces to ensure proper adhesion and longevity of the paint. This includes cleaning, sanding, and repairing any damage to the exterior. We then apply premium-quality paint that withstands harsh weather conditions, ensuring your home stays beautiful for years to come.
-          </p>
-          <p className="mb-8">
-            Let us help you increase your home&apos;s value and curb appeal with expert exterior painting. Contact us to discuss your project and get a free estimate.
-          </p>
-        </ServiceIntro>
+              <ul className="list-disc list-inside text-left space-y-2 w-full max-w-[90%] mx-auto md:mx-0 md:max-w-full">
+                <li>Priming &amp; Stain blocking</li>
+                <li>Scraping</li>
+                <li>Spackling</li>
+                <li>Caulking</li>
+                <li>Foundation Patching and Repair</li>
+              </ul>
+
+              <ul className="list-disc list-inside text-left space-y-2 w-full max-w-[90%] mx-auto md:mx-0 md:max-w-full">
+                <li>Professional Paint Application</li>
+                <li>Spray, Brush &amp; Roll</li>
+                <li>Paint/Stain &amp; Varnish</li>
+                <li>Storm Windows &amp; Shutters</li>
+                <li>Trim Application</li>
+                <li>Metal Applications</li>
+                <li>Porches &amp; Deck Restoration</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="p-8 pl-6 lg:pl-20 lg:pr-20">
+        <p className="pt-4 pb-4">
+          At Portland Painting and Restoration, we prioritize attention to
+          detail and take pride in delivering precise and clean paintwork. Our
+          professionals use advanced techniques and equipment to ensure seamless
+          edges and even coverage, providing a polished and professional
+          appearance to every room.
+        </p>
+        <p className="pt-4 pb-4">
+          Customer satisfaction is our utmost priority. We work closely with you
+          throughout the process, from the initial consultation to the final
+          walk-through, to ensure that your expectations are not only met but
+          exceeded. Our team is dedicated to providing exceptional service,
+          clear communication, and completing the project within the agreed-upon
+          timeline.
+        </p>
+        <p className="pt-4 pb-4">
+          In addition to interior painting, we offer a comprehensive range of
+          services to transform your space fully. Whether you’re looking to
+          update the trim, refresh the cabinets, or add a decorative finish, our
+          experienced professionals have the expertise to handle all your
+          interior painting needs.
+        </p>
+        <p className="pt-4 pb-4">
+          Don’t settle for anything less than the best for your interior
+          painting project. Contact Portland Painting and Restoration today for
+          a consultation and let us show you why we are the preferred choice for
+          interior painting services in Portland, Oregon. Transform your space
+          with our expert craftsmanship and exceptional service.
+        </p>
       </div>
     </>
   );
--- src/app/painting/interior/page.tsx
+++ src/app/painting/interior/page.tsx
@@ -1,6 +1,8 @@
-import ServiceHero from "../../components/ServiceHero";
-import ServiceIntro from "../../components/ServiceIntro";
+import CarouselGallery from "../../components/ImageGallery/CarouselGallery";
+import Image from "next/image";
+import Link from "next/link";
 import type { Metadata } from "next";
+import imageFiles from "@/app/gallery/interior/galleryFiles";
 
 export const metadata: Metadata = {
   title: "Interior Painting Services | Portland Painting & Restoration",
@@ -36,29 +38,140 @@ export const metadata: Metadata = {
 };
 
 export default function InteriorPaintingPage() {
+  const imageSet1 = imageFiles.slice(0, 12);
+  const imageSet2 = imageFiles.slice(12, 24);
   return (
-
     <>
       <div>
-        {/* Hero Section */}
-        <ServiceHero
-          title="Interior Painting Services"
-          subtitle="Transforming the inside of your home with vibrant, lasting colors."
-          backgroundImage="/gallery/interior1.jpeg"
-        />
+        <div className="p-8 pl-6 lg:pl-20 lg:pr-20">
+          <div className="flex items-center space-x-3 bg-gray-100 py-2 px-4 rounded-full max-w-fit mb-4">
+            <div className="flex items-center justify-center w-8 h-6 bg-white rounded-full">
+              <Image
+                src="/gallery/leaf.svg"
+                alt="Portland Painting and Restoration Logo"
+                width={16}
+                height={16}
+              />
+            </div>
+            <p className="text-sm font-medium text-gray-600">
+              Get a free estimate today
+              <Link
+                href="/estimate"
+                className="underline hover:no-underline hover:text-gray-800 transition ml-1"
+              >
+                here!
+              </Link>
+            </p>
+          </div>
+          <h1
+            style={{
+              fontFamily: '"Helvetica Neue", Arial, sans-serif',
+              fontWeight: 500,
+              lineHeight: "1.2",
+            }}
+            className="font-sans font-medium text-4xl lg:text-7xl leading-[1.2]"
+          >
+            Interior Residential Painting
+          </h1>
+          <p className="pb-4 pt-4">
+            Looking for top-quality interior painting services in Portland,
+            Oregon? Look no further than Portland Painting and Restoration. With
+            our team of skilled professionals and a commitment to excellence, we
+            are your go-to choice for transforming the interior of your home or
+            business.
+          </p>
+          <p className="pb-4 pt-4">
+            At Portland Painting and Restoration, we understand that the
+            interior of your property is a reflection of your style and
+            personality. Whether you’re looking to refresh the walls with a new
+            coat of paint or completely revamp the color scheme, our expert
+            painters are here to bring your vision to life. In addition to our
+            exceptional painting services, we also offer comprehensive carpentry
+            services to enhance your interior further, ensuring that every
+            detail of your space is crafted to perfection.
+          </p>
+        </div>
+      </div>
+      <CarouselGallery images={imageSet1} />
+      <div className="p-8 pl-6 lg:pl-20 lg:pr-20 light-green-bg">
+        <h2 className="font-sans font-medium text-4xl lg:text-4xl leading-[1.2] text-center pb-8">
+          Interior Painting Services
+        </h2>
+        <div className="max-w-4xl mx-auto pb-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <ul className="list-disc list-inside text-left space-y-2 w-full max-w-[90%] mx-auto md:mx-0 md:max-w-full">
+              <li>Masking &amp; Protection</li>
+              <li>Paint/Stain/Varnish</li>
+              <li>Wallpaper Removal</li>
+              <li>Complete Wall Preparation</li>
+              <li>Drywall repairs</li>
+            </ul>
 
-        {/* Intro Section */}
-        <ServiceIntro heading="Our Interior Painting Services">
-          <p className="mb-8">
-            A fresh coat of paint can completely change the look and feel of your living spaces. Our interior painting services are designed to bring new life to your walls, ceilings, and trim with precision and care. We work with you to select the perfect colors and finishes to match your style and enhance your home&apos;s interior.
-          </p>
-          <p className="mb-8">
-            From prep work to the final coat, we take care of every detail to ensure a smooth and durable finish. Our team prioritizes cleanliness and minimal disruption, making sure your home remains comfortable during the painting process.
-          </p>
-          <p className="mb-8">
-            Whether you&apos;re updating a single room or giving your entire home a makeover, we&apos;re here to help. Contact us to schedule a consultation and see how we can bring your vision to life.
-          </p>
-        </ServiceIntro>
+            <ul className="list-disc list-inside text-left space-y-2 w-full max-w-[90%] mx-auto md:mx-0 md:max-w-full">
+              <li>Complete Wood Preparation</li>
+              <li>Priming Walls/Wood</li>
+              <li>Caulking</li>
+              <li>Spackling</li>
+              <li>Professional Product Application</li>
+            </ul>
+
+            <ul className="list-disc list-inside text-left space-y-2 w-full max-w-[90%] mx-auto md:mx-0 md:max-w-full">
+              <li>Paint/Stain/Varnish</li>
+              <li>Spray, Brush &amp; Roll</li>
+              <li>Multiple Color Applications</li>
+              <li>Cabinets &amp; Doors</li>
+              <li>High-end Mill pack finishing</li>
+              <li>No “VOC” Paints</li>
+            </ul>
+          </div>
+        </div>
+        <p className="pb-4 pt-4">
+          Our interior painting process begins with thorough preparation to
+          ensure a flawless finish. We start by carefully inspecting the
+          surfaces, repairing any imperfections such as cracks or holes, and
+          applying a high-quality primer. This step is vital as it creates a
+          smooth and even base for the paint, resulting in a
+          professional-looking result.
+        </p>
+        <p className="pb-12 pt-4">
+          We offer a wide range of paint options, including low VOC and
+          eco-friendly paints, to cater to your specific needs and preferences.
+          Our skilled painters are experienced in working with different
+          textures and finishes, from matte to glossy, to achieve the desired
+          look and feel for your space.
+        </p>
+      </div>
+      <CarouselGallery bgcolor="#e8f2ec" images={imageSet2} />
+      <div className="p-8 pl-6 lg:pl-20 lg:pr-20">
+        <p className="pt-4 pb-4">
+          At Portland Painting and Restoration, we prioritize attention to
+          detail and take pride in delivering precise and clean paintwork. Our
+          professionals use advanced techniques and equipment to ensure seamless
+          edges and even coverage, providing a polished and professional
+          appearance to every room.
+        </p>
+        <p className="pt-4 pb-4">
+          Customer satisfaction is our utmost priority. We work closely with you
+          throughout the process, from the initial consultation to the final
+          walk-through, to ensure that your expectations are not only met but
+          exceeded. Our team is dedicated to providing exceptional service,
+          clear communication, and completing the project within the agreed-upon
+          timeline.
+        </p>
+        <p className="pt-4 pb-4">
+          In addition to interior painting, we offer a comprehensive range of
+          services to transform your space fully. Whether you’re looking to
+          update the trim, refresh the cabinets, or add a decorative finish, our
+          experienced professionals have the expertise to handle all your
+          interior painting needs.
+        </p>
+        <p className="pt-4 pb-4">
+          Don’t settle for anything less than the best for your interior
+          painting project. Contact Portland Painting and Restoration today for
+          a consultation and let us show you why we are the preferred choice for
+          interior painting services in Portland, Oregon. Transform your space
+          with our expert craftsmanship and exceptional service.
+        </p>
       </div>
     </>
   );
