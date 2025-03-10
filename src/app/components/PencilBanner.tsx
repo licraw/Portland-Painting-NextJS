4dd6dfc5610ff0fd0ae593491fd2a3c2e3ef6152
Dylan C <dcrawshaw@fender.com>
2025-03-10 13:52:15 -0700

    update

--- src/app/components/PencilBanner.tsx
+++ src/app/components/PencilBanner.tsx
@@ -6,8 +6,35 @@ import { FiPhone } from "react-icons/fi";
 type PencilBannerProps = {
   ctaAction: string;
 };
+declare function gtag(
+  event: string,
+  action: string,
+  params: {
+    send_to: string;
+    value: number;
+    currency: string;
+    event_callback: () => void;
+  }
+): void;
+
 
 export default function PencilBanner({ ctaAction }: PencilBannerProps) {
+ 
+  
+  const handleConversionClick = (event: React.MouseEvent<HTMLDivElement>): void => {
+    const callback = () => {
+      window.location.href = "tel:5032367003";
+    };
+  
+    gtag('event', 'conversion', {
+      send_to: 'AW-1016197559/BVgECM6X7agaELfjx-QD',
+      value: 1.0,
+      currency: 'USD',
+      event_callback: callback,
+    });
+    event.preventDefault();
+    console.log("Conversion event sent");
+  };
   return (
     <div className="bg-green-700 text-white py-2 px-4 text-sm font-medium">
       {/* Container with responsive horizontal padding */}
@@ -32,7 +59,7 @@ export default function PencilBanner({ ctaAction }: PencilBannerProps) {
         </div>
 
         {/* Right side: Phone number */}
-        <div>
+        <div onClick={handleConversionClick}>
           <a
             href="tel:5032367003"
             className="flex items-center gap-1 hover:text-gray-300 transition"
